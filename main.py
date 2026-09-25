e9059f70f14c0762cb4780d1850c0212b3acd323
Carlos Contini <continicarlos.alb@gmail.com>
2026-09-25 18:35:05 -0300

    borre todo y agregue la primera linea

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-asd = 8
-
-#vamos a la primera modificacion 
-
-
-#Esto es una prubea para iniciar un git y conectarlo con un repo en gitHub
-
-
-#Voy a hacer mi primera modificacion en un repo clonado 
+#esta es la primera linea que le corresponde a Marce :D
